96808864b4effbfedb8d03c13814bf42bffde1dc
WackoBeast <vladvlaicu99@gmail.com>
2020-08-23 21:47:50 +0300

    Chaquopy integration complete

--- app/src/main/java/wb61/remuse/MainActivity.java
+++ app/src/main/java/wb61/remuse/MainActivity.java
@@ -1,7 +1,12 @@
 package wb61.remuse;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initPython();
+
+        String text = getPythonHelloWorld();
+        System.out.println(text);
+    }
+
+    private String getPythonHelloWorld() {
+        Python python = Python.getInstance();
+        PyObject pythonFile = python.getModule("helloworldscript");
+        return pythonFile.callAttr("helloworld").toString();
+    }
+
+    private void initPython() {
+        if(!Python.isStarted()){
+            Python.start(new AndroidPlatform(this));
+        }
     }
 }
